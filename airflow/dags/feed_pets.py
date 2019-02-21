5528a57e603bb03d6afbce5277d90998703e5eb6
jtdv01 <jtdv01@gmail.com>
2019-02-21 17:06:06 +1100

    Started skeleton

--- airflow/dags/feed_pets.py
+++ airflow/dags/feed_pets.py
@@ -1,1 +1,20 @@
 # Call a microservice to feed pets. Scheduled 3 times a day
+from airflow.models import DAG
+
+DAG = DAG()
+
+# Fetch cans
+fetch_operator = PythonOperator()
+
+# Try an open
+open_operator = PythonOperator
+
+# Try a serve, pet may refused
+serve_operator = 
+
+# If consumed, mark as done on FeedDiary via Clojure microservice
+mark_as_done = HTTPOperator()
+
+# Analyse FeedDiary postgres
+
+analyse = PostgresOperator
