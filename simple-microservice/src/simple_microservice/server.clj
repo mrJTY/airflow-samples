a4c0a177888ae2b48e1f903cccee5f0693d7e91a
jtdv01 <jtdv01@gmail.com>
2019-03-18 12:05:45 +1100

    Clean up

--- simple-microservice/src/simple_microservice/server.clj
+++ simple-microservice/src/simple_microservice/server.clj
@@ -5,8 +5,6 @@
             [simple-microservice.prepopulate-database :as prepopulate]
             [simple-microservice.service :as service]))
 
-;; This is an adapted service map, that can be started and stopped
-;; From the REPL you can call server/start and server/stop on this service
 (defonce runnable-service (server/create-server service/service))
 
 (defn run-dev
@@ -37,23 +35,3 @@
   (println "\nCreating your server...")
   (prepopulate/prepopulate-database)
   (server/start runnable-service))
-
-;; If you package the service up as a WAR,
-;; some form of the following function sections is required (for io.pedestal.servlet.ClojureVarServlet).
-
-;;(defonce servlet  (atom nil))
-;;
-;;(defn servlet-init
-;;  [_ config]
-;;  ;; Initialize your app here.
-;;  (reset! servlet  (server/servlet-init service/service nil)))
-;;
-;;(defn servlet-service
-;;  [_ request response]
-;;  (server/servlet-service @servlet request response))
-;;
-;;(defn servlet-destroy
-;;  [_]
-;;  (server/servlet-destroy @servlet)
-;;  (reset! servlet nil))
-
